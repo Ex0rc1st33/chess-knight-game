ea5dafd1f2d239b2d67a117131245c5e3e9708d6
Kálny Zalán <kalny.zalan7@gmail.com>
2021-05-14 01:34:50 +0200

    Added extra features to the UI

--- src/main/java/chesspuzzle/ChessPuzzleController.java
+++ src/main/java/chesspuzzle/ChessPuzzleController.java
@@ -1,15 +1,15 @@
 package chesspuzzle;
 
+import chesspuzzle.model.*;
+import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.*;
 import javafx.scene.shape.Circle;
-import chesspuzzle.model.BoardState;
-import chesspuzzle.model.Direction;
-import chesspuzzle.model.Position;
+import javafx.scene.text.Font;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,54 +42,64 @@ public class ChessPuzzleController {
     @FXML
     private void initialize() {
         createBoard();
-        createPieces();
+        createKnights();
         setSelectablePositions();
         showSelectablePositions();
     }
 
     private void createBoard() {
+        table.getStyleClass().add("table");
         for (int i = 0; i < table.getRowCount(); i++) {
             for (int j = 0; j < table.getColumnCount(); j++) {
-                var square = createSquare();
-                table.add(square, j, i);
+                var tile = createTile(i, j);
+                table.add(tile, j, i);
+
             }
         }
     }
 
-    private StackPane createSquare() {
-        var square = new StackPane();
-        square.getStyleClass().add("square");
-        square.setOnMouseClicked(this::handleMouseClick);
-        return square;
+    private StackPane createTile(int i, int j) {
+        var tile = new StackPane();
+        tile.getStyleClass().add("tile");
+        tile.setOnMouseClicked(this::handleMouseClick);
+        Image image = new Image(getClass().getResourceAsStream("/" + ((i + j) % 2 == 0 ? "light" : "dark") + "_tile.png"));
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(image, null, null, null, backgroundSize);
+        Background background = new Background(backgroundImage);
+        tile.setBackground(background);
+        return tile;
     }
 
-    private void createPieces() {
+    private void createKnights() {
         int size = model.getKnights().length;
         for (int i = 0; i < size; i++) {
-            model.positionObjectProperty(i).addListener(this::piecePositionChange);
-            System.out.println(model.positionObjectProperty(i));
-            var piece = createPiece(Color.valueOf("black"));
-            getSquare(model.getKnight(i).getPosition()).getChildren().add(piece);
+            model.positionObjectProperty(i).addListener(this::knightPositionChange);
+            var knight = createKnight(model.getKnight(i).getColor());
+            getTile(model.getKnight(i).getPosition()).getChildren().add(knight);
         }
     }
 
-    private Circle createPiece(Color color) {
-        var piece = new Circle(25);
-        piece.setFill(color);
-        return piece;
+    private Label createKnight(chesspuzzle.model.Color color) {
+        /*Image image = new Image(getClass().getResourceAsStream("/" + color + "_knight.png"));
+        ImageView view = new ImageView();
+        view.setImage(image);
+        view.setFitHeight(100);
+        view.setPreserveRatio(true);
+        view.setSmooth(true);
+        return view;*/
+        var knight = new Label((color == Color.BLACK ? "\u265E" : "\u2658"));
+        knight.setFont(new Font(100));
+        knight.setTextFill(javafx.scene.paint.Color.BLACK);
+        return knight;
     }
 
     @FXML
     private void handleMouseClick(MouseEvent event) {
-        var square = (StackPane) event.getSource();
-        var row = GridPane.getRowIndex(square);
-        var col = GridPane.getColumnIndex(square);
-        var position = new Position(row, col);
-        handleClickOnSquare(position);
+        var tile = (StackPane) event.getSource();
+        handleClickOnTile(new Position(GridPane.getRowIndex(tile), GridPane.getColumnIndex(tile)));
     }
 
-    private void handleClickOnSquare(Position position) {
-        System.out.println(selectionPhase);
+    private void handleClickOnTile(Position position) {
         switch (selectionPhase) {
             case SELECT_FROM -> {
                 if (selectablePositions.contains(position)) {
@@ -99,18 +109,20 @@ public class ChessPuzzleController {
             }
             case SELECT_TO -> {
                 if (selectablePositions.contains(position)) {
-                    var pieceNumber = model.getKnightIndex(selected).getAsInt();
-                    var direction = Direction.of(position.getRow() - selected.getRow(), position.getCol() - selected.getCol());
-                    model.move(pieceNumber, direction);
+                    int knightIndex = model.getKnightIndex(selected).getAsInt();
+                    Direction direction = Direction.of(position.getRow() - selected.getRow(), position.getCol() - selected.getCol());
+                    model.move(knightIndex, direction);
+                    deselectSelectedPosition();
+                    alterSelectionPhase();
+                } else if (position.equals(selected)) {
                     deselectSelectedPosition();
                     alterSelectionPhase();
                 }
             }
         }
-        int size = model.getKnights().length;
-        System.out.println("");
-        for (int i = 0; i < size; i++) {
-            System.out.println(model.positionObjectProperty(i));
+        if (model.isGoal()) {
+            Platform.exit();
+            System.exit(0);
         }
     }
 
@@ -126,28 +138,40 @@ public class ChessPuzzleController {
         showSelectedPosition();
     }
 
+    private void showSelectedPosition() {
+        var tile = getTile(selected);
+        tile.getStyleClass().add("selected");
+        /*var arrow = new Label("\u25B2");
+        arrow.setFont(new Font(30));
+        arrow.setTextFill(javafx.scene.paint.Color.BLUE);
+        tile.getChildren().add(arrow);
+        System.out.println(tile.getChildren().size());*/
+
+    }
+
     private void deselectSelectedPosition() {
         hideSelectedPosition();
         selected = null;
     }
 
-    private void showSelectedPosition() {
-        var square = getSquare(selected);
-        square.getStyleClass().add("selected");
-    }
-
     private void hideSelectedPosition() {
-        var square = getSquare(selected);
-        square.getStyleClass().remove("selected");
+        var tile = getTile(selected);
+        tile.getStyleClass().remove("selected");
     }
 
     private void setSelectablePositions() {
         selectablePositions.clear();
         switch (selectionPhase) {
-            case SELECT_FROM -> selectablePositions.addAll(model.getKnightPositions());
+            case SELECT_FROM -> {
+                for (Knight knight : model.getKnights()) {
+                    if (knight.getColor() == model.getNextColor()) {
+                        selectablePositions.add(knight.getPosition());
+                    }
+                }
+            }
             case SELECT_TO -> {
-                var knightIndex = model.getKnightIndex(selected).getAsInt();
-                for (var direction : model.getLegalMoves(knightIndex)) {
+                int knightIndex = model.getKnightIndex(selected).getAsInt();
+                for (Direction direction : model.getLegalMoves(knightIndex)) {
                     selectablePositions.add(selected.getTarget(direction));
                 }
             }
@@ -156,19 +180,41 @@ public class ChessPuzzleController {
 
     private void showSelectablePositions() {
         for (var selectablePosition : selectablePositions) {
-            var square = getSquare(selectablePosition);
-            square.getStyleClass().add("selectable");
+            var tile = getTile(selectablePosition);
+            tile.getStyleClass().add("selectable");
+            if (selectionPhase == SelectionPhase.SELECT_FROM) {
+                var arrow = new Label("\u25B2");
+                arrow.setFont(new Font(30));
+                arrow.setTextFill(javafx.scene.paint.Color.GREEN);
+                tile.getChildren().add(arrow);
+            } else {
+                Circle circle = createCircle();
+                tile.getChildren().add(circle);
+            }
         }
     }
 
     private void hideSelectablePositions() {
         for (var selectablePosition : selectablePositions) {
-            var square = getSquare(selectablePosition);
-            square.getStyleClass().remove("selectable");
+            var tile = getTile(selectablePosition);
+            tile.getStyleClass().remove("selectable");
+            if (selectionPhase == SelectionPhase.SELECT_TO) {
+                tile.getChildren().remove(tile.getChildren().size() - 1);
+            } else {
+                tile.getChildren().remove(0);
+            }
         }
     }
 
-    private StackPane getSquare(Position position) {
+    private Circle createCircle() {
+        Circle circle = new Circle();
+        circle.setRadius(20);
+        circle.setFill(javafx.scene.paint.Color.LIGHTGRAY);
+        circle.setOpacity(0.7);
+        return circle;
+    }
+
+    private StackPane getTile(Position position) {
         for (var child : table.getChildren()) {
             if (GridPane.getRowIndex(child) == position.getRow() && GridPane.getColumnIndex(child) == position.getCol()) {
                 return (StackPane) child;
@@ -177,12 +223,22 @@ public class ChessPuzzleController {
         throw new AssertionError();
     }
 
-    private void piecePositionChange(ObservableValue<? extends Position> observable, Position oldPosition, Position newPosition) {
-        System.out.println("ASDASDASDASDA");
-        StackPane oldSquare = getSquare(oldPosition);
-        StackPane newSquare = getSquare(newPosition);
-        newSquare.getChildren().addAll(oldSquare.getChildren());
-        oldSquare.getChildren().clear();
+    private void knightPositionChange(ObservableValue<? extends Position> observable, Position oldPosition, Position newPosition) {
+        StackPane oldTile = getTile(oldPosition);
+        StackPane newTile = getTile(newPosition);
+        newTile.getChildren().addAll(oldTile.getChildren());
+        //newTile.getChildren().remove(newTile.getChildren().size() - 1);
+        oldTile.getChildren().clear();
+    }
+
+    @FXML
+    private void handleReset() {
+        table.getChildren().removeAll(table.getChildren());
+        model = new BoardState();
+        selectionPhase = SelectionPhase.SELECT_FROM;
+        selectablePositions.clear();
+        selected = null;
+        initialize();
     }
 
 }
