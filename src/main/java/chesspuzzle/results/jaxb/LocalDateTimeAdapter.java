48627b010b7c1b1ed61cd216f47d3ba1a0e728bb
kalnyzalan <kalny.zalan7@gmail.com>
2021-05-18 12:44:48 +0200

    Finished documentation

--- src/main/java/chesspuzzle/results/jaxb/LocalDateTimeAdapter.java
+++ src/main/java/chesspuzzle/results/jaxb/LocalDateTimeAdapter.java
@@ -1,17 +1,30 @@
 package chesspuzzle.results.jaxb;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import jakarta.xml.bind.annotation.adapters.XmlAdapter;
 
+/**
+ * Adapter class to convert {@code LocalDateTime} objects to {@code String} objects and vice versa,
+ * to help serializing objects to XML, and deserializing objects from XML.
+ */
 public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
 
+    /**
+     * {@return the {@code LocalDateTime} object corresponding to the specified {@code String}}
+     *
+     * @param s the {@code String} object to be converted
+     */
     @Override
     public LocalDateTime unmarshal(String s) {
         return LocalDateTime.parse(s);
     }
 
+    /**
+     * {@return the {@code String} object corresponding to the specified {@code LocalDateTime}}
+     *
+     * @param localDateTime the {@code LocalDateTime} object to be converted
+     */
     @Override
     public String marshal(LocalDateTime localDateTime) {
         return localDateTime.toString();
